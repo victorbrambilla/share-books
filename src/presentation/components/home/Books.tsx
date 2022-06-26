8dfa3aa1045ad108dec6a095c97283256a2725c5
victor <victor-brambilla@hotmail.com>
2022-06-25 22:45:33 -0400

    feat(add): create books section

---
+++ src/presentation/components/home/Books.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { BookModel } from '@/domain/models';
+import { useReactHorizontalScrollingDrag } from '@/presentation/hooks/use-react-horizontal-scrolling-drag';
+import { Box, Typography } from '@mui/material';
+import { Slider } from '../slider/Slider';
+import { BookCard } from '../bookCard/BookCard';
+
+interface IProps {
+  books: BookModel[];
+}
+export const Books = ({ books }: IProps) => {
+  const { dragStart, dragStop, handleDrag } = useReactHorizontalScrollingDrag();
+
+  return (
+    <Box
+      sx={{
+        padding: '24px',
+      }}
+    >
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
+        }}
+      >
+        <Typography color={'secondary'} fontSize={'22px'} fontWeight={'bold'}>
+          Livros
+        </Typography>
+        <Typography
+          color={'secondary'}
+          fontSize={'18px'}
+          fontWeight={'bold'}
+          sx={{ cursor: 'pointer' }}
+          variant='subtitle1'
+        >
+          Ver todos
+        </Typography>
+      </Box>
+      <Box>
+        {books && (
+          <Slider
+            dragStop={dragStop}
+            onMouseDown={() => dragStart}
+            onMouseUp={() => dragStop}
+            onMouseMove={handleDrag}
+          >
+            {books.map((book: BookModel) => (
+              <BookCard book={book} />
+            ))}
+          </Slider>
+        )}
+      </Box>
+    </Box>
+  );
+};
--- src/presentation/pages/home/Home.tsx
+++ src/presentation/pages/home/Home.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import { Banner } from '@/presentation/components/home/Banner';
 import { Institutes } from '@/presentation/components/home/Institutes';
+import { BookModel, InstituteModel } from '@/domain/models';
+import { Books } from '@/presentation/components/home/Books';
 
-export const HomePage = (): JSX.Element => {
+interface IProps {
+  books: BookModel[];
+  institutes: InstituteModel[];
+}
+
+export const HomePage = ({ institutes, books }: IProps): JSX.Element => {
   return (
     <div>
       {' '}
       <div>
         <Banner />
-        <Institutes />
+        <Institutes institutes={institutes} />
+        <Books books={books} />
       </div>
     </div>
   );
